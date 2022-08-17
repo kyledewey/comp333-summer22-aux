4067eb0e1449b2b382ff430a960469c55d77cff0
Kyle Dewey <kyle.dewey@csun.edu>
2022-08-16 21:33:21 -0700

    stuff from today

--- swift/enums.swift
+++ swift/enums.swift
@@ -63,3 +63,96 @@ case .Nil:
 // 2.) Encode the above tree with these enums
 // 3.) Write a function that recursively computes the sum of all the values
 //     in the tree.  The sum of a leaf if 0.
+
+// public class ListNode {
+//   public int value;
+//                     Node = Node | null;
+//   public Node rest; // rest is null if the rest of the list is empty
+// }
+
+indirect enum BinaryTree {
+    case Root(BinaryTree, Int, BinaryTree)
+    case Leaf
+}
+
+let tree: BinaryTree =
+  BinaryTree.Root(BinaryTree.Root(BinaryTree.Leaf, 2, BinaryTree.Leaf),
+                  5,
+                  BinaryTree.Root(BinaryTree.Leaf, 7, BinaryTree.Leaf));
+
+//
+// Type theory: theory of types.  int, String, boolean, ...
+//  |
+//  Equivalent to logic.
+//  |
+//  Product types (holds multiple things at the same time) - AND
+//    |
+//    Classes (multiple instance variables)
+//  |
+//  Sum types (you have exactly one thing among a choice of things) - OR
+//    |
+//    Algebraic data types
+//
+
+// monomorphization
+// public interface BinaryTree {
+//   public int sum();
+// }
+// public class Root implements BinaryTree {
+//   public final int value;
+//   public final BinaryTree left;
+//   public final BinaryTree right;
+//   public Root(final int value, final BinaryTree left, final BinaryTree right) {
+//     this.value = value;
+//     this.left = left;
+//     this.right = right;
+//   }
+//   public int sum() {
+//     return value + left.sum() + right.sum();
+//   }
+// }
+// public class Leaf implements BinaryTree {
+//   public int sum() {
+//     return 0;
+//   }
+// }
+//
+// arr:
+// 0: address of case 0
+// 1: address of case 1
+// 2: address of case 2
+// ...
+//
+// jump arr[my_value]
+//
+// switch my_value {
+//   case 0:
+//   case 1:
+//   case 2: ...
+// }
+func sum(node: BinaryTree) -> Int {
+    switch node {
+    case let .Root(.Leaf, 5, .Root(let innerValue, _, _)):
+        // if (node instanceof Root) {
+        //   Root nodeAsRoot = (Root)node;
+        //   if (nodeAsRoot.left instanceof Leaf &&
+        //       nodeAsRoot.value == 5 &&
+        //       nodeAsRoot.right == Root) {
+        //     int innerValue = ((Root)nodeAsRoot.right).value;
+        //   
+        
+    case let .Root(leftTree, value, rightTree):
+        return value + sum(node: leftTree) + sum(node: rightTree)
+    case .Leaf: return 0;
+    }
+}
+
+print(sum(node: tree))
+
+// switch sum{
+// case .Branch( let root1, let val, let root2):
+//     return(root1.sum + val + root2.sum)
+    
+// case .Leaf:
+//     return 0
+// }
